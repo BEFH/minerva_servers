669325f1610aa21f7bc88ebfca22f5cd7f5d0878
Brian Fulton-Howard <fultonh1@gmail.com>
2022-09-06 16:41:04 -0400

    update for minerva usage

--- singularity/run_singularity.sh
+++ singularity/run_singularity.sh
@@ -3,47 +3,38 @@
 # See also https://www.rocker-project.org/use/singularity/
 
 # Main parameters for the script with default values
-PORT=${PORT:-8787}
-USER=$(whoami)
-PASSWORD=${PASSWORD:-notsafe}
-TMPDIR=${TMPDIR:-tmp}
-CONTAINER="rstudio_latest.sif"  # path to singularity container (will be automatically downloaded)
+RSTUDIO_PORT=${RSTUDIO_RSTUDIO_PORT:-8787}
+RSTUDIO_USER=$(whoami)
+RSTUDIO_PASSWORD=${RSTUDIO_PASSWORD:-notsafe}
+RSTUDIO_TMPDIR=${RSTUDIO_TMPDIR:-tmp}
+CONTAINER="/sc/arion/work/$USER/rstudio_latest.sif"  # path to singularity container (will be automatically downloaded)
 
 # Set-up temporary paths
-RSTUDIO_TMP="${TMPDIR}/$(echo -n $CONDA_PREFIX | md5sum | awk '{print $1}')"
+RSTUDIO_TMP="${RSTUDIO_TMPDIR}/$(echo -n $RSTUDIO_CONDA_PREFIX | md5sum | awk '{print $1}')"
 mkdir -p $RSTUDIO_TMP/{run,var-lib-rstudio-server,local-share-rstudio}
 
-R_BIN=$CONDA_PREFIX/bin/R
-PY_BIN=$CONDA_PREFIX/bin/python
+RSTUDIO_R_BIN=$RSTUDIO_CONDA_PREFIX/bin/R
+RSTUDIO_PY_BIN=$RSTUDIO_CONDA_PREFIX/bin/python
 
 if [ ! -f $CONTAINER ]; then
-	singularity build --fakeroot $CONTAINER Singularity
+	singularity pull $CONTAINER oras://ghcr.io/befh/rstudio-server-conda:latest
 fi
 
-if [ -z "$CONDA_PREFIX" ]; then
+if [ -z "$RSTUDIO_CONDA_PREFIX" ]; then
   echo "Activate a conda env or specify \$CONDA_PREFIX"
   exit 1
 fi
 
-echo "Starting rstudio service on port $PORT ..."
-singularity exec \
+echo "Starting rstudio service on port $RSTUDIO_PORT ..."
+singularity run \
 	--bind $RSTUDIO_TMP/run:/run \
 	--bind $RSTUDIO_TMP/var-lib-rstudio-server:/var/lib/rstudio-server \
 	--bind /sys/fs/cgroup/:/sys/fs/cgroup/:ro \
 	--bind database.conf:/etc/rstudio/database.conf \
 	--bind rsession.conf:/etc/rstudio/rsession.conf \
 	--bind $RSTUDIO_TMP/local-share-rstudio:/home/rstudio/.local/share/rstudio \
-	--bind ${CONDA_PREFIX}:${CONDA_PREFIX} \
+	--bind ${RSTUDIO_CONDA_PREFIX}:${RSTUDIO_CONDA_PREFIX} \
 	--bind $HOME/.config/rstudio:/home/rstudio/.config/rstudio \
         `# add additional bind mount required for your use-case` \
 	--bind /data:/data \
-	--env CONDA_PREFIX=$CONDA_PREFIX \
-	--env RSTUDIO_WHICH_R=$R_BIN \
-	--env RETICULATE_PYTHON=$PY_BIN \
-	--env PASSWORD=$PASSWORD \
-	--env PORT=$PORT \
-	--env USER=$USER \
-	rstudio_latest.sif \
-	/init.sh
-
-
+	rstudio_latest.sif
