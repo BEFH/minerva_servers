6872b189e7c633f2d88fa088575501dbb276882c
Brian Fulton-Howard <brian.fulton-howard@mssm.edu>
2022-09-07 10:39:00 -0400

    livability changes

--- singularity/run_singularity.sh
+++ singularity/run_singularity.sh
@@ -3,27 +3,30 @@
 # See also https://www.rocker-project.org/use/singularity/
 
 # Main parameters for the script with default values
-RSTUDIO_PORT=${RSTUDIO_RSTUDIO_PORT:-8787}
-RSTUDIO_USER=$(whoami)
-RSTUDIO_PASSWORD=${RSTUDIO_PASSWORD:-notsafe}
-RSTUDIO_TMPDIR=${RSTUDIO_TMPDIR:-tmp}
+export RSTUDIO_PORT=${RSTUDIO_PORT:-8787}
+export RSTUDIO_USER=$(whoami)
+export RSTUDIO_PASSWORD=${RSTUDIO_PASSWORD:-notsafe}
+export RSTUDIO_TMPDIR=${RSTUDIO_TMPDIR:-tmp}
+export RSTUDIO_CONDA_PREFIX=$CONDA_PREFIX
 CONTAINER="/sc/arion/work/$USER/rstudio_latest.sif"  # path to singularity container (will be automatically downloaded)
 
 # Set-up temporary paths
-RSTUDIO_TMP="${RSTUDIO_TMPDIR}/$(echo -n $RSTUDIO_CONDA_PREFIX | md5sum | awk '{print $1}')"
-mkdir -p $RSTUDIO_TMP/{run,var-lib-rstudio-server,local-share-rstudio}
+export RSTUDIO_TMP="${RSTUDIO_TMPDIR}/$(echo -n $CONDA_PREFIX | md5sum | awk '{print $1}')"
+mkdir -p $RSTUDIO_TMP/{run,var-lib-rstudio-server,local-share-rstudio,tmp}
 
-RSTUDIO_R_BIN=$RSTUDIO_CONDA_PREFIX/bin/R
-RSTUDIO_PY_BIN=$RSTUDIO_CONDA_PREFIX/bin/python
+export RSTUDIO_R_BIN=$CONDA_PREFIX/bin/R
+export RSTUDIO_PY_BIN=$CONDA_PREFIX/bin/python
 
 if [ ! -f $CONTAINER ]; then
 	singularity pull $CONTAINER oras://ghcr.io/befh/rstudio-server-conda:latest
 fi
 
-if [ -z "$RSTUDIO_CONDA_PREFIX" ]; then
+if [ -z "$CONDA_PREFIX" ]; then
   echo "Activate a conda env or specify \$CONDA_PREFIX"
   exit 1
 fi
+
+echo $RSTUDIO_TMP
 
 echo "Starting rstudio service on port $RSTUDIO_PORT ..."
 singularity run \
@@ -33,8 +36,7 @@ singularity run \
 	--bind database.conf:/etc/rstudio/database.conf \
 	--bind rsession.conf:/etc/rstudio/rsession.conf \
 	--bind $RSTUDIO_TMP/local-share-rstudio:/home/rstudio/.local/share/rstudio \
-	--bind ${RSTUDIO_CONDA_PREFIX}:${RSTUDIO_CONDA_PREFIX} \
+	--bind ${CONDA_PREFIX}:${CONDA_PREFIX} \
+`#  --bind ${RSTUDIO_TMP}/tmp:/tmp/rstudio-server` \
 	--bind $HOME/.config/rstudio:/home/rstudio/.config/rstudio \
-        `# add additional bind mount required for your use-case` \
-	--bind /data:/data \
-	rstudio_latest.sif
+	$CONTAINER
