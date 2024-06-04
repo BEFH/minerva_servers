8c35f422ceee8f56b2fc3551db4a898023051569
Brian Fulton-Howard <fultonh1@gmail.com>
2024-06-04 15:18:56 -0400

    further startup fix

--- vscode_rocky/start_code.sh
+++ vscode_rocky/start_code.sh
@@ -4,7 +4,7 @@
 
 __conda_setup="$('/opt/conda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
-    eval "\$__conda_setup"
+    eval "$__conda_setup"
 else
     if [ -f "/opt/conda/etc/profile.d/conda.sh" ]; then
         . "/opt/conda/etc/profile.d/conda.sh"
