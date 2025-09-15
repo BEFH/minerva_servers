c89ed1dd8b1dea3361faf5d1885ea9a612f50d39
Brian Fulton-Howard <fultonh1@gmail.com>
2025-09-15 12:21:41 -0400

    testing for positron

---
+++ positron_rocky/start_positron.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Activate conda and mamba
+
+__conda_setup="$('/opt/conda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/opt/conda/etc/profile.d/conda.sh" ]; then
+        . "/opt/conda/etc/profile.d/conda.sh"
+    else
+        export PATH="/opt/conda/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+
+if [ -f "/opt/conda/etc/profile.d/mamba.sh" ]; then
+    . "/opt/conda/etc/profile.d/mamba.sh"
+fi
+
+# Activate the conda environment
+
+deactcount=0
+while [[ -n "$CONDA_PREFIX" && $deactcount -lt 5 ]]; do
+    conda deactivate
+    ((deactcount+=1))
+done
+
+conda activate $RS_CONDAENV
+
+# start vscode
+
+exec code "${@}"
